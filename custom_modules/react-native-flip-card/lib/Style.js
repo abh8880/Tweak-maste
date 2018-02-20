3c7f0103047dd3b4c4d38db0d00f290d4501ad84
Jaffrey Joy <jaffreyjoy@gmail.com>
2018-02-20 08:29:59 +0530

    custom modules ADDED

---
+++ custom_modules/react-native-flip-card/lib/Style.js
@@ -0,0 +1,17 @@
+'use strict'
+import {StyleSheet} from 'react-native'
+
+export default StyleSheet.create({
+  flipCard: {
+  },
+
+  face: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+
+  back: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+  }
+})
