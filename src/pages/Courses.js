6e490975fef31b5e502ca32582b93909d785c7bf
gladydennyson <gladydennyson184@gmail.com>
2018-03-06 21:19:02 +0530

    Previous commit errors fixed

--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -71,7 +71,7 @@ export default class VerticalStackLayout extends Component {
 
            <View style={styles.innerBox1}>
               <Image
-                  source={require('../../icons/12.png')}
+                  source={require('../../icons/2.png')}
                   style={{width: '50%', height: '50%', top:'5%',}}
                 />
 
@@ -125,7 +125,7 @@ export default class VerticalStackLayout extends Component {
 
             <View style={styles.innerBox1}>
               <Image
-                  source={require('../../icons/24.png')}
+                  source={require('../../icons/6.png')}
                   style={{width: '50%', height: '50%', top:'5%'}}
                 />
 
