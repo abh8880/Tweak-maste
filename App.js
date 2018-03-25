efc4d39f5946db779ab16a0c30cf0c6f927ca08d
Gary Mendonca <mendonca_gary@ymail.com>
2018-03-25 15:32:08 +0530

    remove splash screen

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import SplashScreen from 'react-native-splash-screen'
+//import SplashScreen from 'react-native-splash-screen'
 
 import React, { Component } from 'react';
 import {
@@ -15,7 +15,7 @@ export default class App extends Component<{}> {
    componentDidMount() {
       // do stuff while splash screen is shown
         // After having done stuff (such as async tasks) hide the splash screen
-        SplashScreen.hide();
+      //  SplashScreen.hide();
     }
   constructor(){
     super();
